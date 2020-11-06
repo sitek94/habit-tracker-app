d96075e4c572a8845ca851c66a200c9ec0180e15
sitek94 <msitkowski94@gmail.com>
2020-11-06 11:26:21 +0100

    Update days of the week
    There is no more need to store them as an array of objects.

--- src/data/days-of-the-week.js
+++ src/data/days-of-the-week.js
@@ -1,11 +1,11 @@
-const daysOfTheWeek = [
-  { id: 0, day: 'Monday', checked: false },
-  { id: 1, day: 'Tuesday', checked: false },
-  { id: 2, day: 'Wednesday', checked: false },
-  { id: 3, day: 'Thursday', checked: false },
-  { id: 4, day: 'Friday', checked: false },
-  { id: 5, day: 'Saturday', checked: false },
-  { id: 6, day: 'Sunday', checked: false },
+export const daysOfTheWeek = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
 ];
 
 export default daysOfTheWeek;

2dd2c89ea311740e6ff78f549e212b3d7c656c11
sitek94 <msitkowski94@gmail.com>
2020-11-06 19:17:40 +0100

    Create Dialog context

---
+++ src/components/dialog/dialog-context.js
@@ -0,0 +1,48 @@
+import  { createContext, useContext, useState, useRef } from 'react';
+
+import DialogContainer from './Dialog';
+
+const DialogContext = createContext();
+
+const DialogProvider = ({ children }) => {
+  const [dialogs, setDialogs] = useState([]);
+
+  const openDialog = props => {
+    const dialog = { ...props, open: true };
+    setDialogs(dialogs => [...dialogs, dialog]);
+  }
+
+  const closeDialog =() => {
+    setDialogs(dialogs => {
+      const latestDialog= dialogs.pop();
+
+      if (!latestDialog) return dialogs;
+      if (latestDialog.onClose) latestDialog.onClose();
+
+      return [...dialogs].concat({ ...latestDialog, open: false });
+    })
+  }
+
+  const contextValue = useRef({openDialog, closeDialog});
+
+  return (
+    <DialogContext.Provider value={contextValue.current}>
+      {children}
+      {dialogs.map((dialog, i) => (
+        <DialogContainer key={i} {...dialog} onClose={closeDialog} />
+      ))}
+    </DialogContext.Provider>
+  );
+};
+
+function useDialog() {
+  const context = useContext(DialogContext);
+
+  if (context === undefined) {
+    throw new Error('useDialog must be used within HabitsProvider');
+  }
+
+  return context;
+}
+
+export { DialogProvider, useDialog };

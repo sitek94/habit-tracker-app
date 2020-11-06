c8d4d0a23b510e1f2af5fe4a2637bd9e847c5fc8
sitek94 <msitkowski94@gmail.com>
2020-11-06 16:27:57 +0100

    Set text-align to center

--- src/components/empty-state/EmptyState.js
+++ src/components/empty-state/EmptyState.js
@@ -43,7 +43,7 @@ const EmptyState = ({
 
   if (type === 'page') {
     return (
-      <AbsoluteCenter>
+      <AbsoluteCenter textAlign="center">
         {image && (
           <Box
             clone

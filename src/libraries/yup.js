2575876bcffd8db085c4c6af4bead904b25b2c9c
sitek94 <msitkowski94@gmail.com>
2020-11-06 16:05:13 +0100

    Add user schema, rename habit schema

--- src/libraries/yup.js
+++ src/libraries/yup.js
@@ -1,17 +1,31 @@
 import { yupResolver } from '@hookform/resolvers/yup';
 
-import { object, string, array } from 'yup';
+import { object, string, array, ref } from 'yup';
 
-const newHabitSchema = object().shape({
+const habitSchema = object().shape({
   title: string().required('Title is required.'),
   description: string().notRequired(),
   trackedDays: array().required('You have to select at least one day.'),
 });
 
-const editedHabitSchema = object().shape({
-  title: string().notRequired(),
-  description: string().notRequired(),
-  trackedDays: array().notRequired(),
-});
+const userSchema = object().shape({
+  email: string()
+    .email('Email address is invalid')
+    .required('Email address is required.'),
 
-export { yupResolver, newHabitSchema, editedHabitSchema };
+  emailConfirmation: string()
+    .email('Email address confirmation is invalid')
+    .oneOf([ref('email'), null], `Email addresses don't match.`)
+    .required('Email address confirmation is required.'),
+
+  password: string()
+    .min(6, 'Password must be at least 6 characters.')
+    .required('You have to enter the password.'),
+
+  passwordConfirmation: string()
+    .min(6, 'Password must be at least 6 characters.')
+    .oneOf([ref('password'), null], `Passwords don't match.`)
+    .required('You have to enter the password confirmation.'),
+})
+
+export { yupResolver, habitSchema, userSchema };

ca50bf41292533a0a21d62e3b94226399f7ce93c
sitek94 <msitkowski94@gmail.com>
2020-11-06 16:28:11 +0100

    Create separate schemas for sign-in and sign-up

--- src/libraries/yup.js
+++ src/libraries/yup.js
@@ -2,30 +2,50 @@ import { yupResolver } from '@hookform/resolvers/yup';
 
 import { object, string, array, ref } from 'yup';
 
+/**
+ * HABIT
+ */
+const title = string().required('Title is required.');
+const description = string().notRequired();
+const trackedDays = array().required('You have to select at least one day.');
+
 const habitSchema = object().shape({
-  title: string().required('Title is required.'),
-  description: string().notRequired(),
-  trackedDays: array().required('You have to select at least one day.'),
+  title,
+  description,
+  trackedDays,
 });
 
-const userSchema = object().shape({
-  email: string()
-    .email('Email address is invalid')
-    .required('Email address is required.'),
+/**
+ * USER
+ */
+const email = string()
+  .email('Email address is invalid')
+  .required('Email address is required.');
 
-  emailConfirmation: string()
-    .email('Email address confirmation is invalid')
-    .oneOf([ref('email'), null], `Email addresses don't match.`)
-    .required('Email address confirmation is required.'),
+const emailConfirmation = string()
+  .email('Email address confirmation is invalid')
+  .oneOf([ref('email'), null], `Email addresses don't match.`)
+  .required('Email address confirmation is required.');
 
-  password: string()
-    .min(6, 'Password must be at least 6 characters.')
-    .required('You have to enter the password.'),
+const password = string()
+  .min(6, 'Password must be at least 6 characters.')
+  .required('You have to enter the password.');
 
-  passwordConfirmation: string()
-    .min(6, 'Password must be at least 6 characters.')
-    .oneOf([ref('password'), null], `Passwords don't match.`)
-    .required('You have to enter the password confirmation.'),
+const passwordConfirmation = string()
+  .min(6, 'Password must be at least 6 characters.')
+  .oneOf([ref('password'), null], `Passwords don't match.`)
+  .required('You have to enter the password confirmation.');
+
+const signInSchema = object().shape({
+  email,
+  password
+});
+
+const signUpSchema = object().shape({
+  email,
+  emailConfirmation,
+  password,
+  passwordConfirmation,
 })
 
-export { yupResolver, habitSchema, userSchema };
+export { yupResolver, habitSchema, signInSchema, signUpSchema };

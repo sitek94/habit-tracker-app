827f20dfc567f2333f6ebdd206f2bdabd602dbc6
sitek94 <msitkowski94@gmail.com>
2020-11-06 19:15:01 +0100

    Add autoHideDuration prop

--- src/components/snackbar/Snackbar.js
+++ src/components/snackbar/Snackbar.js
@@ -3,7 +3,13 @@ import PropTypes from 'prop-types';
 import { Snackbar as MuiSnackbar } from '@material-ui/core';
 import { Alert } from '@material-ui/lab';
 
-const Snackbar = ({ severity = 'info', open = false, onClose, message }) => {
+const Snackbar = ({
+  severity = 'info',
+  open = false,
+  onClose,
+  message,
+  autoHideDuration = 2000,
+}) => {
   const handleClose = (event, reason) => {
     if (reason === 'clickaway') {
       return;
@@ -13,7 +19,11 @@ const Snackbar = ({ severity = 'info', open = false, onClose, message }) => {
   };
 
   return (
-    <MuiSnackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+    <MuiSnackbar
+      open={open}
+      autoHideDuration={autoHideDuration}
+      onClose={handleClose}
+    >
       <Alert
         variant="filled"
         elevation={6}
@@ -31,6 +41,7 @@ Snackbar.propTypes = {
   severity: PropTypes.oneOf(['info', 'error', 'success', 'warning']),
   open: PropTypes.bool,
   message: PropTypes.string.isRequired,
+  autoHideDuration: PropTypes.number,
 
   // Event handlers
   onClose: PropTypes.func.isRequired,

57ed57c40c1902254c9cac8cdd3cd6c84f9a66f3
sitek94 <msitkowski94@gmail.com>
2020-11-07 17:00:29 +0100

    Use Table instead of List to render habits

--- src/pages/habits/Habits.js
+++ src/pages/habits/Habits.js
@@ -1,59 +1,57 @@
-import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import {
-  Avatar,
-  IconButton,
-  List,
-  ListItem,
-  ListItemAvatar,
-  ListItemSecondaryAction,
-  ListItemText,
-} from '@material-ui/core';
+import { Box, Fab, Table, TableBody, TableContainer } from '@material-ui/core';
+import { Add as AddIcon } from '@material-ui/icons';
 
-import {
-  Delete as DeleteIcon,
-  Edit as EditIcon,
-  Folder as FolderIcon,
-} from '@material-ui/icons';
-
+import EmptyState from 'components/empty-state';
+import HabitItem from 'components/habit-item';
 import { useHabits } from 'features/habits';
-
-const HabitItem = ({ habit: { id, title, description } }) => {
-  const history = useHistory();
-
-  const handleEditClick = () => {
-    history.push(`/dashboard/habits/${id}`);
-  };
-
-  return (
-    <ListItem>
-      <ListItemAvatar>
-        <Avatar>
-          <FolderIcon />
-        </Avatar>
-      </ListItemAvatar>
-      <ListItemText primary={title} secondary={description} />
-      <IconButton onClick={handleEditClick}>
-        <EditIcon />
-      </IconButton>
-      <ListItemSecondaryAction>
-        <IconButton edge="end" aria-label="delete">
-          <DeleteIcon />
-        </IconButton>
-      </ListItemSecondaryAction>
-    </ListItem>
-  );
-};
+import { ReactComponent as EmptyBox } from 'svgs/empty-box.svg';
 
 const HabitsPage = () => {
-  const habits = useHabits();
+  const { habits } = useHabits();
 
+  // User doesn't have any habits yet, render a page with
+  // a link to `Add habit` page
+  if (habits.length === 0) {
+    return (
+      <EmptyState
+        image={<EmptyBox />}
+        title="No habits"
+        description="It looks like you don't have any habits yet"
+        button={
+          <Fab
+            variant="extended"
+            color="primary"
+            component={Link}
+            to="/dashboard/add-habit"
+          >
+            <Box clone mr={1}>
+              <AddIcon />
+            </Box>
+            Create habit
+          </Fab>
+        }
+      />
+    );
+  }
+
+  // Render user's habits
   return (
-    <List>
-      {habits.map(habit => (
-        <HabitItem key={habit.id} habit={habit} />
-      ))}
-    </List>
+    <TableContainer>
+      <Table>
+        <TableBody>
+          {habits.map(({ id, title, trackedDays }) => (
+            <HabitItem
+              key={id}
+              id={id}
+              title={title}
+              trackedDays={trackedDays}
+            />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 

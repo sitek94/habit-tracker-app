a2ace04ce5f39713fa267a213d2621d4ff5abd0a
sitek94 <msitkowski94@gmail.com>
2020-11-08 19:55:11 +0100

    Add isSameOrBefore plugin

--- src/libraries/dayjs.js
+++ src/libraries/dayjs.js
@@ -1,9 +1,12 @@
 import dayjs from 'dayjs';
+
 import weekday from 'dayjs/plugin/weekday';
 import updateLocale from 'dayjs/plugin/updateLocale';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
 
 dayjs.extend(weekday);
 dayjs.extend(updateLocale);
+dayjs.extend(isSameOrBefore);
 
 dayjs.updateLocale('en', {
   weekStart: 1,

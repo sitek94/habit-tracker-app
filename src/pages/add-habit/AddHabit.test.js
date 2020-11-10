4abefb3777bef1a8a474e464f1e2a00535d2dd4c
sitek94 <msitkowski94@gmail.com>
2020-11-10 16:32:03 +0100

    Move files to different directory

--- src/pages/add-habit/AddHabit.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-import AddHabitPage from './AddHabit';
-
-it('renders without crashing', () => {
-  render(<AddHabitPage />);
-
-  expect(screen.getByText('Create a new habit')).toBeInTheDocument();
-});

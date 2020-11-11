f14311ccd5710dd5d801f73abe23bb1b2d9a635e
sitek94 <msitkowski94@gmail.com>
2020-11-11 19:51:26 +0100

    Store formats that are used in database

---
+++ src/data/database-formats.js
@@ -0,0 +1,13 @@
+const databaseFormats = {
+  checkmark: {
+    date: 'DD-MM-YYYY',
+    value: {
+      completed: 1,
+      skipped: 2,
+      failed: 3,
+    }
+  }
+}
+
+export default databaseFormats;
+

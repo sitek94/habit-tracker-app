c9c7115afdc919603ef3956bc9b5c129ffd3f275
sitek94 <msitkowski94@gmail.com>
2020-11-11 19:21:04 +0100

    Extract fetching habits logic to `useHabits` hook

--- src/features/habits/habits-list-page/HabitsListPage.js
+++ src/features/habits/habits-list-page/HabitsListPage.js
@@ -1,60 +1,14 @@
-import { useEffect, useState } from 'react';
-
 import { Table, TableBody, TableContainer } from '@material-ui/core';
 
 import Loader from 'components/loader';
 import ErrorPage from 'components/error-page';
-
-import { useFirebase } from 'services/firebase';
-
 import NoHabits from '../no-habits';
-import HabitItem from './habit-item';
+import HabitItem from './HabitItem';
+
+import { useHabits } from '../useHabits';
 
 const HabitList = () => {
-  const { db, user } = useFirebase();
-
-  const [habits, setHabits] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-
-  useEffect(() => {
-    const fetchHabits = async () => {
-      setIsLoading(true);
-
-      try {
-        setIsError(false);
-
-        const snapshot = await db
-          .ref('habits')
-          .orderByChild('user')
-          .equalTo(user.uid)
-          .once('value');
-
-        let fetchedHabits = snapshot.val();
-
-        // User doesn't have any habits
-        if (!fetchedHabits) {
-          return;
-        }
-
-        // Fetched habits is an object so we need to convert it to an array
-        fetchedHabits = Object.entries(fetchedHabits).map(([id, habit]) => ({
-          id,
-          ...habit,
-        }));
-
-        setHabits(fetchedHabits);
-      } catch (error) {
-        console.log(error);
-
-        setIsError(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchHabits();
-  }, [db, user]);
+  const [{ habits, isLoading, isError }, setHabits] = useHabits();
 
   const deleteHabit = habitId => {
     setHabits(habits.filter(h => h.id !== habitId));

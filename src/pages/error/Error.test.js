3d3d71f2559b47f4474d9296277128f1b5f7a55e
sitek94 <msitkowski94@gmail.com>
2020-11-11 18:38:43 +0100

    Change directory structure

--- src/pages/error/Error.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-import ErrorPage from './Error';
-
-it('renders without crashing', () => {
-  render(<ErrorPage />);
-
-  expect(screen.getByText('Something went wrong')).toBeInTheDocument();
-});

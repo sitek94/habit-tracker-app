3e6b19fe72e50d1fc80d81012cbe1772cfa19af2
sitek94 <msitkowski94@gmail.com>
2020-11-12 20:37:19 +0100

    Remove old files

--- src/features/habits/add-habit-page/AddHabitPage.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-import AddHabitPage from './AddHabitPage';
-
-it('renders without crashing', () => {
-  render(<AddHabitPage />);
-
-  expect(screen.getByText('Create a new habit')).toBeInTheDocument();
-});

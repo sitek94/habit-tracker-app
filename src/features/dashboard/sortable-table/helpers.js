bb2c8bf13abc629a86c58d3c98782d88e39eb8f6
sitek94 <msitkowski94@gmail.com>
2020-11-14 19:39:08 +0100

    Add getComparator and descendingComparator functions

---
+++ src/features/dashboard/sortable-table/helpers.js
@@ -0,0 +1,15 @@
+export function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+export function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}

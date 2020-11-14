264009cd9ca447317c3d248cace6afa0c3b03bc4
sitek94 <msitkowski94@gmail.com>
2020-11-14 21:00:20 +0100

    Add Sortable Table component, 100% coverage tests

--- src/features/dashboard/sortable-table/__tests__/sortable-table-head.test.js
+++ src/features/dashboard/sortable-table/__tests__/sortable-table-head.test.js
@@ -4,15 +4,23 @@ import SortableTableHead from '../sortable-table-head';
 
 beforeEach(cleanup);
 
-describe('<DashboardList />', () => {
-   it('renders table head when order is asc', () => {
-    render(<SortableTableHead order="asc" orderBy="name" onRequestSort={() => {}} />);
+// function customRender(ui) {
+//   render(<table>{ui}</table>);
+// }
+
+describe('<SortableTableHead />', () => {
+  it('renders table head when order is asc', () => {
+    render(
+      <SortableTableHead order="asc" orderBy="name" onRequestSort={() => {}} />
+    );
 
     expect(screen.getByText(/Habit name/)).toBeTruthy();
   });
 
   it('renders table head when order is desc', () => {
-    render(<SortableTableHead order="desc" orderBy="name" onRequestSort={() => {}} />);
+    render(
+      <SortableTableHead order="desc" orderBy="name" onRequestSort={() => {}} />
+    );
 
     expect(screen.getByText(/Habit name/)).toBeTruthy();
   });
@@ -20,10 +28,16 @@ describe('<DashboardList />', () => {
   it('renders and accepts onClick event', () => {
     const handleRequestSort = jest.fn();
 
-    render(<SortableTableHead order="asc" orderBy="name" onRequestSort={handleRequestSort} />);
+    render(
+      <SortableTableHead
+        order="asc"
+        orderBy="name"
+        onRequestSort={handleRequestSort}
+      />
+    );
 
     fireEvent.click(screen.getByTestId('position'));
 
     expect(handleRequestSort).toHaveBeenCalled();
-  })
+  });
 });

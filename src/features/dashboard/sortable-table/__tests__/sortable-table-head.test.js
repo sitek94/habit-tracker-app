6c8613b6d2fa855cce90197f83897ccbc39b4e27
sitek94 <msitkowski94@gmail.com>
2020-11-14 19:40:52 +0100

    Add Sortable Table Head component, write tests

---
+++ src/features/dashboard/sortable-table/__tests__/sortable-table-head.test.js
@@ -0,0 +1,29 @@
+import { render, screen, fireEvent, cleanup } from 'utils/test-utils';
+
+import SortableTableHead from '../sortable-table-head';
+
+beforeEach(cleanup);
+
+describe('<DashboardList />', () => {
+   it('renders table head when order is asc', () => {
+    render(<SortableTableHead order="asc" orderBy="name" onRequestSort={() => {}} />);
+
+    expect(screen.getByText(/Habit name/)).toBeTruthy();
+  });
+
+  it('renders table head when order is desc', () => {
+    render(<SortableTableHead order="desc" orderBy="name" onRequestSort={() => {}} />);
+
+    expect(screen.getByText(/Habit name/)).toBeTruthy();
+  });
+
+  it('renders and accepts onClick event', () => {
+    const handleRequestSort = jest.fn();
+
+    render(<SortableTableHead order="asc" orderBy="name" onRequestSort={handleRequestSort} />);
+
+    fireEvent.click(screen.getByTestId('position'));
+
+    expect(handleRequestSort).toHaveBeenCalled();
+  })
+});

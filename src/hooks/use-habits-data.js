29d72c1c3c491cddce6b1bda1f2b801e3bf309b0
sitek94 <msitkowski94@gmail.com>
2020-11-14 17:46:50 +0100

    Rename `useHabits` to `useHabitsData`

--- src/hooks/use-habits-data.js
+++ src/hooks/use-habits-data.js
@@ -2,13 +2,13 @@ import { useEffect, useState } from 'react';
 
 import { useFirebase } from 'services/firebase';
 
-export function useHabits() {
+export function useHabitsData() {
   const { db, user } = useFirebase();
 
   const [habits, setHabits] = useState([]);
   const [status, setStatus] = useState('idle');
   const [error, setError] = useState(null);
-
+  
   useEffect(() => {
     let _isMounted = true;
 
@@ -21,16 +21,18 @@ export function useHabits() {
       .on(
         'value',
         snapshot => {
+          let fetchedHabits = [];
+
           // Update habits only if there was a value in database
-          if (!snapshot.exists()) return;
-
-          let fetchedHabits = snapshot.val();
-
-          // Fetched habits is an object so we need to convert it to an array
-          fetchedHabits = Object.entries(fetchedHabits).map(([id, habit]) => ({
-            id,
-            ...habit,
-          }));
+          if (snapshot.exists()) {
+            fetchedHabits = snapshot.val();
+            
+            // Fetched habits is an object so we need to convert it to an array
+            fetchedHabits = Object.entries(fetchedHabits).map(([id, habit]) => ({
+              id,
+              ...habit,
+            }));
+          }
 
           if (_isMounted) {
             setHabits(fetchedHabits);

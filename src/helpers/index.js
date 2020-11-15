691a04477ab65ee879e1cf07848a2734b77e87fc
sitek94 <msitkowski94@gmail.com>
2020-11-15 20:54:58 +0100

    Start implementing charts

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -49,3 +49,56 @@ export function getNextCheckmarkValue(currentValue) {
 export function getNextCheckmarkLabel(currentValue) {
   return getCheckmarkLabel(getNextCheckmarkValue(currentValue));
 }
+
+/**
+ * Returns an array of checkmark like objects. Each object has two keys:
+ * 
+ * day - date of the checkmark
+ * value - average of completed and failed checkmarks values
+ * 
+ * @param {Array} arr an array of habits
+ */
+export function aggregateHabitsCheckmarks(arr) {
+  let obj = {};
+
+  arr.forEach((habit) => {
+    concatCheckmarkValueByDay(obj, habit.checkmarks);
+  });
+
+  return averageCheckmarkValues(obj);
+}
+
+/**
+ * Concatenate values of checkmarks from source array with corresponding day key
+ * in target object.
+ * 
+ * @param {Object} targetObj target object
+ * @param {Array} sourceArr array of checkmarks
+ */
+function concatCheckmarkValueByDay(targetObj, checkmarksArr) {
+  checkmarksArr.forEach(({ day, value }) => {
+    if (targetObj[day] === undefined) {
+      // If the key doesn't exists create one
+      targetObj[day] = [value];
+    } else {
+      // If the key already exists concat the value of checkmark
+      targetObj[day] = targetObj[day].concat(value);
+    }
+  });
+}
+
+/**
+ * Calculates average value of checkmarks values for each day.
+ * 
+ * @param {Object} obj object with day-values key-value pairs
+ */
+function averageCheckmarkValues(obj) {
+  return  Object.entries(obj).map(([day, values]) => {
+    return {
+      day,
+      value: values.reduce((acc, cur) => acc + cur, 0) / values.length
+    };
+  });
+}
+
+

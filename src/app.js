8e1c9cb7a6dc145cc423c263f6162bdd4368adca
sitek94 <msitkowski94@gmail.com>
2020-11-18 14:14:17 +0100

    Refactor: Render either AuthenticatedApp or UnathenticatedApp

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+import { useAuth } from 'context/auth-context';
+import { FullPageSpinner } from 'components/lib';
+
+const AuthenticatedApp = React.lazy(() => import('./authenticated-app'));
+const UnathenticatedApp = React.lazy(() =>  import('./unauthenticated-app'));
+
+function App() {
+  const { user } = useAuth();
+
+  return (
+    <React.Suspense fallback={<FullPageSpinner />}>
+      {user ? <AuthenticatedApp /> : <UnathenticatedApp />}
+    </React.Suspense>
+  );
+}
+
+export default App;

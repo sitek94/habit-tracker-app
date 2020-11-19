54dd6dea2c95fb4a1a12c72afb2f2ff23ae1d251
sitek94 <msitkowski94@gmail.com>
2020-11-19 16:08:59 +0100

    delete old files

--- src/pages/landing/landing.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-import LadingPage from './LadingPage';
-
-it('renders without crashing', () => {
-  render(<LadingPage />);
-
-  expect(screen.getByText('Habit Tracker App')).toBeInTheDocument();
-});

509a2c54ec6d4d450af8270fa999813b07f8156a
sitek94 <msitkowski94@gmail.com>
2020-11-21 14:04:34 +0100

    cleanup

--- src/pages/not-found/not-found.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-import NotFoundPage from './NotFoundPage';
-
-it('renders without crashing', () => {
-  render(<NotFoundPage onAuthProviderClick={() => {}} />);
-
-  expect(screen.getByText(`Page doesn’t exist`)).toBeInTheDocument();
-});

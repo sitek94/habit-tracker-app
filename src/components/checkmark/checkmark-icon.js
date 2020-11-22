3c6f00d97fe584bc62b59f549eaf142fe8b6c59e
sitek94 <msitkowski94@gmail.com>
2020-11-22 14:33:05 +0100

    update checkmark logic, little refactor

--- src/components/checkmark/checkmark-icon.js
+++ src/components/checkmark/checkmark-icon.js
@@ -1,5 +1,4 @@
 import PropTypes from 'prop-types';
-
 import { green } from '@material-ui/core/colors';
 import {
   CheckBox as CheckBoxIcon,
@@ -12,12 +11,18 @@ import { COMPLETED, FAILED, EMPTY } from 'data/constants';
 function CheckmarkIcon({ value }) {
   switch (value) {
     case COMPLETED:
-      return <CheckBoxIcon style={{ color: green[500] }} data-testid="checkmark-completed" />;
+      return (
+        <CheckBoxIcon
+          style={{ color: green[500] }}
+          data-testid="checkmark-completed"
+        />
+      );
 
     case FAILED:
-      return <CheckboxFailedIcon color="secondary" data-testid="checkmark-failed" />;
+      return (
+        <CheckboxFailedIcon color="secondary" data-testid="checkmark-failed" />
+      );
 
-    case null:
     case EMPTY:
     default:
       return <CheckBoxBlankIcon data-testid="checkmark-empty" />;
@@ -28,4 +33,4 @@ CheckmarkIcon.propTypes = {
   value: PropTypes.number,
 };
 
-export default CheckmarkIcon;
+export { CheckmarkIcon };
--- src/data/constants.js
+++ src/data/constants.js
@@ -2,6 +2,6 @@ export const DATE_FORMAT = 'YYYY-MM-DD';
 
 export const COMPLETED = 1;
 export const FAILED = -1;
-export const EMPTY = 2;
+export const EMPTY = null;
 
 export const CHECKMARK_VALUES = [EMPTY, COMPLETED, FAILED];

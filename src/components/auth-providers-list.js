4ed585ee02d04ca07086ee55bdf1e09bcc6e574b
sitek94 <msitkowski94@gmail.com>
2020-11-28 10:18:15 +0100

    refactor: remove redundant function

--- src/components/auth-providers-list.js
+++ src/components/auth-providers-list.js
@@ -26,19 +26,18 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-// Creates a theme with primary color set to corresponding auth provider color
-const createAuthTheme = (providerColor) =>
-  createMuiTheme({
-    palette: {
-      primary: providerColor,
-    },
-  });
-
 const AuthProviderButton = ({ providerColor, ...rest }) => {
   const classes = useStyles();
 
+  // Create a theme with primary color set to corresponding auth provider color
+  const providerTheme = createMuiTheme({
+    palette: {
+      primary: providerColor
+    }
+  })
+
   return (
-    <ThemeProvider theme={createAuthTheme(providerColor)}>
+    <ThemeProvider theme={providerTheme}>
       <Button
         classes={{
           root: classes.root,

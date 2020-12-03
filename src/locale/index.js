3b168a403fb88a806330512304931e8966b657f9
sitek94 <msitkowski94@gmail.com>
2020-12-03 21:10:13 +0100

    add exports

--- src/locale/index.js
+++ src/locale/index.js
@@ -1,1 +1,4 @@
-export { LocaleProvider, useLocale, locales } from './locale-context';
+export { MainLocaleProvider } from './main-locale-context';
+export { UserLocaleProvider } from './user-locale-context';
+export { locales, defaultLocale } from './locales';
+export { useLocale } from './use-locale';

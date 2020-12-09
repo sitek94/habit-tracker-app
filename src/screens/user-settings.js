c171de70015268a732723a2f9c988053f0c8eaae
sitek94 <msitkowski94@gmail.com>
2020-12-09 01:40:32 +0100

    improve responsiveness

--- src/screens/user-settings.js
+++ src/screens/user-settings.js
@@ -1,27 +1,16 @@
-import { Box, Paper } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import Tab from '@material-ui/core/Tab';
-import Tabs from '@material-ui/core/Tabs';
+import * as React from 'react';
+import SwipeableViews from 'react-swipeable-views';
+import { Box, Paper, Tab, Tabs } from '@material-ui/core';
+import { AccountTab } from 'components/account-tab';
+import { AppearanceTab } from 'components/appearance-tab';
+import { PerformanceTab } from 'components/performance-tab';
 import {
   AccountCircle as AccountCircleIcon,
   Equalizer as EqualizerIcon,
   Palette as PaletteIcon,
-  Security as SecurityIcon,
 } from '@material-ui/icons';
-import { AccountTab } from 'components/account-tab';
-import { AppearanceTab } from 'components/appearance-tab';
-import { PerformanceTab } from 'components/performance-tab';
-import * as React from 'react';
-import SwipeableViews from 'react-swipeable-views';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    height: '100%',
-    margin: theme.spacing(4),
-    maxWidth: 600,
-  },
-}));
+// Available tabs
 const tabs = [
   {
     key: 'account',
@@ -40,56 +29,72 @@ const tabs = [
     icon: <PaletteIcon />,
     label: 'Appearance',
   },
-
-  {
-    key: 'security',
-    icon: <SecurityIcon />,
-    label: 'Security',
-  },
 ];
-export default function UserSettingsScreen() {
-  const classes = useStyles();
 
+/**
+ * User Settings Screen
+ * 
+ * The settings are displayed as swipeable tabs. The settings that are available
+ * are: account, performance, appearance.
+ */
+export default function UserSettingsScreen() {
+  // Selected tab
   const [selectedTab, setSelectedTab] = React.useState(0);
   const handleTabChange = (event, newValue) => {
     setSelectedTab(newValue);
   };
+
+  // SwipeableViews index change handler
   const handleIndexChange = (index) => {
     setSelectedTab(index);
   };
 
   return (
-    <Paper className={classes.root}>
-      <Tabs
-        value={selectedTab}
-        onChange={handleTabChange}
-        variant="fullWidth"
-        indicatorColor="primary"
-        textColor="primary"
+    <Box
+      sx={{
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        maxWidth: 600,
+      }}
+    >
+      <Box
+        component={Paper}
+        sx={{ width: '100%', height: { xs: '100%', sm: 'auto' } }}
       >
-        {tabs.map(({ key, icon, label }, index) => {
-          return <Tab key={key} icon={icon} label={label} value={index} />;
-        })}
-      </Tabs>
-      <Box sx={{ m: 2 }}>
-        <SwipeableViews 
-          // Fixes broken animation when changing view for the first time.
-          // https://github.com/oliviertassinari/react-swipeable-views/issues/599
-          containerStyle={{
-            transition: 'transform 0.35s cubic-bezier(0.15, 0.3, 0.25, 1) 0s'
-          }}
-          index={selectedTab}
-          onChangeIndex={handleIndexChange}
+        {/* Tabs */}
+        <Tabs
+          value={selectedTab}
+          onChange={handleTabChange}
+          variant="fullWidth"
+          indicatorColor="primary"
+          textColor="primary"
         >
-          <AccountTab />
+          {tabs.map(({ key, icon, label }, index) => {
+            return <Tab key={key} icon={icon} label={label} value={index} />;
+          })}
+        </Tabs>
 
-          <PerformanceTab />
+        {/* Views */}
+        <Box sx={{ m: { xs: 0, sm: 2 } }}>
+          <SwipeableViews
+            // Fixes broken animation when changing view for the first time.
+            // https://github.com/oliviertassinari/react-swipeable-views/issues/599
+            containerStyle={{
+              transition: 'transform 0.35s cubic-bezier(0.15, 0.3, 0.25, 1) 0s',
+            }}
+            index={selectedTab}
+            onChangeIndex={handleIndexChange}
+          >
+            <AccountTab />
 
-          <AppearanceTab />
-          <div>ADSD</div>
-        </SwipeableViews>
+            <PerformanceTab />
+
+            <AppearanceTab />
+          </SwipeableViews>
+        </Box>
       </Box>
-    </Paper>
+    </Box>
   );
 }
 

4bb75da72a6fb805327cdf6d03760b21b036c112
sitek94 <msitkowski94@gmail.com>
2020-12-09 23:48:34 +0100

    update responsiveness

--- src/screens/dashboard.js
+++ src/screens/dashboard.js
@@ -1,67 +1,40 @@
 import * as React from 'react';
-import {
-  Box,
-  Container,
-  Grid,
-  Hidden,
-  makeStyles,
-  Paper,
-} from '@material-ui/core';
-import { HabitsTable } from 'components/habits-table';
-import { FullPageSpinner } from 'components/lib';
-import { UserScores } from 'components/user-scores';
-import { WeekPicker } from 'components/week-picker';
-import {
-  eachDayOfInterval,
-  endOfWeek,
-  lightFormat,
-  startOfWeek,
-} from 'date-fns';
 import { useCheckmarks } from 'api/checkmarks';
 import { useHabits } from 'api/habits';
 import { NoHabitsScreen } from 'screens/no-habits';
 import { useUserData } from 'context/user-config-context';
 import { useLocale } from 'locale';
 import { WeekBarChart } from 'components/week-bar-chart';
-// Styles
-const useStyles = makeStyles((theme) => ({
-  container: {
-    // maxWidth: theme.breakpoints.values.lg,
-    height: '100%',
-    padding: theme.spacing(4),
-  },
-
-  paper: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    overflow: 'auto',
-  },
-  fixedHeight: {
-    height: 300,
-    overflow: 'hidden',
-  },
-  padding: {
-    padding: theme.spacing(2),
-  },
-}));
+import { HabitsTable } from 'components/habits-table';
+import { FullPageSpinner } from 'components/lib';
+import { UserScores } from 'components/user-scores';
+import { WeekPicker } from 'components/week-picker';
+import {
+  Box,
+  Container,
+  Grid,
+  Hidden,
+  Paper,
+} from '@material-ui/core';
+import {
+  eachDayOfInterval,
+  endOfWeek,
+  lightFormat,
+  startOfWeek,
+} from 'date-fns';
 
 /**
  * Dashboard Screen
  *
  * In the dashboard there are:
- *
  * - Habits table - user can change the completion state of the habits.
- *
  * - User Scores - shows the user's performance for last week, current week and today.
- *
  * - Week Picker - user can pick the week which will update the table.
  *
  * ### TODO: All time performance chart or something like that.
  */
 function DashboardScreen() {
   const locale = useLocale();
-  const classes = useStyles();
 
   // Habits data
   const {
@@ -102,109 +75,126 @@ function DashboardScreen() {
   }
 
   const barChart = (
-    <TopRowPaper>
+    <SmallPaper>
       <WeekBarChart
         dates={selectedDates}
         checkmarks={selectedDatesCheckmarks}
         habitsCount={habits.length}
       />
-    </TopRowPaper>
+    </SmallPaper>
   );
 
   const weekPicker = (
-    <TopRowPaper>
+    <SmallPaper>
       <WeekPicker
         selectedDate={selectedDate}
         onChange={(newDate) => setSelectedDate(newDate)}
       />
-    </TopRowPaper>
+    </SmallPaper>
   );
 
   const habitsTable = (
-    <Paper className={classes.paper}>
+    <LargePaper>
       <HabitsTable
         checkmarks={checkmarks}
         habits={habits}
         dates={selectedDates}
       />
-    </Paper>
+    </LargePaper>
   );
 
   const userScores = (
-    <TopRowPaper>
+    <SmallPaper>
       <UserScores checkmarks={checkmarks} goal={performanceGoal} />
-    </TopRowPaper>
+    </SmallPaper>
   );
 
   // Render
   return (
-    // <div className={classes.container}>
-    <Container disableGutters>
-
-      {/* Mobile screens */}
-      <Hidden smUp>
-        <Grid container spacing={1}>
-          <Grid item xs={12}>
-            {weekPicker}
-          </Grid>
-          <Grid item xs={12}>
-            {habitsTable}
-          </Grid>
-          <Grid item xs={12}>
-            {userScores}
-          </Grid>
-          <Grid item xs={12}>
-            {barChart}
-          </Grid>
-        </Grid>
-      </Hidden>
-
-      {/* Small screens */}
-      <Hidden smDown mdUp>
-        <Box sx={{ p: 1 }}>
+    <Box sx={{ height: '100%' }} clone>
+      <Container disableGutters>
+        {/* Mobile screens */}
+        <Hidden smUp>
           <Grid container spacing={1}>
-            <Grid item sm={6}>
-              {userScores}
-            </Grid>
-            <Grid item sm={6}>
+            <Grid item xs={12}>
               {weekPicker}
             </Grid>
-            <Grid item sm={12}>
+            <Grid item xs={12}>
               {habitsTable}
             </Grid>
-            <Grid item sm={12}>
+            <Grid item xs={12}>
+              {userScores}
+            </Grid>
+            <Grid item xs={12}>
               {barChart}
             </Grid>
           </Grid>
-        </Box>
-      </Hidden>
+        </Hidden>
 
-      {/* Large screens up */}
-      <Hidden mdDown>
-        <Box sx={{ p: 2 }}>
-          <Grid container spacing={2}>
-            <Grid item md={4}>
-              {barChart}
+        {/* Small screens */}
+        <Hidden smDown mdUp>
+          <Box sx={{ p: 1 }}>
+            <Grid container spacing={1}>
+              <Grid item sm={6}>
+                {userScores}
+              </Grid>
+              <Grid item sm={6}>
+                {weekPicker}
+              </Grid>
+              <Grid item sm={12}>
+                {habitsTable}
+              </Grid>
+              <Grid item sm={12}>
+                {barChart}
+              </Grid>
             </Grid>
-            <Grid item md={4}>
-              {userScores}
+          </Box>
+        </Hidden>
+
+        {/* Large screens up */}
+        <Hidden mdDown>
+          <Box sx={{ p: 2 }}>
+            <Grid container spacing={2}>
+              <Grid item md={4}>
+                {barChart}
+              </Grid>
+              <Grid item md={4}>
+                {userScores}
+              </Grid>
+              <Grid item md={4}>
+                {weekPicker}
+              </Grid>
+              <Grid item md={12}>
+                {habitsTable}
+              </Grid>
             </Grid>
-            <Grid item md={4}>
-              {weekPicker}
-            </Grid>
-            <Grid item md={12}>
-              {habitsTable}
-            </Grid>
-          </Grid>
-        </Box>
-      </Hidden>
-    </Container>
+          </Box>
+        </Hidden>
+      </Container>
+    </Box>
   );
 }
 
-function TopRowPaper({ children, sx }) {
+function LargePaper({ children }) {
   return (
     <Box
+      component={Paper}
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        overflow: 'auto',
+      }}
+    >
+      {children}
+    </Box>
+  );
+}
+
+function SmallPaper({ children }) {
+  return (
+    <Box
+      component={Paper}
       sx={{
         display: 'flex',
         flexDirection: 'column',
@@ -212,9 +202,7 @@ function TopRowPaper({ children, sx }) {
         alignItems: 'center',
         height: 330,
         overflow: 'hidden',
-        ...sx,
       }}
-      component={Paper}
     >
       {children}
     </Box>

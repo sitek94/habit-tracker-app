b6ee234dee3afc9bbe426c6c71c9054b33ef2908
sitek94 <msitkowski94@gmail.com>
2020-12-13 14:31:08 +0100

    move config to firebase.js

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -4,11 +4,20 @@ import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/firestore';
 
-import firebaseConfig from './firebase-config';
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
 
 class Firebase {
   constructor() {
-    firebase.initializeApp(firebaseConfig);
+    firebase.initializeApp(config);
 
     this.firebase = firebase;
     this.auth = firebase.auth();

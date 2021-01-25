134f0fb7d127e881d7edb092453948e75cd5c848
sitek94 <msitkowski94@gmail.com>
2021-01-25 15:50:19 +0100

    refactor:  extract modules from <AuthenticatedApp>

--- src/app/app.js
+++ src/app/app.js
@@ -1,27 +1,19 @@
 import * as React from 'react';
 
 import { useAuth } from 'context/auth-context';
-// import { FullPageSpinner } from 'components/lib';
-import AuthenticatedApp from './authenticated-app';
 import { UnathenticatedApp } from './unathenticated-app';
 import { AuthenticatedAppProviders } from 'context';
-
-// const AuthenticatedApp = React.lazy(() => import('./authenticated-app'));
-// const UnathenticatedApp = React.lazy(() =>  import('./unauthenticated-app'));
+import { AuthenticatedApp } from './authenticated-app';
 
 function App() {
   const { user } = useAuth();
 
-  return (
-    // <React.Suspense fallback={<FullPageSpinner />}>
-    user ? (
-      <AuthenticatedAppProviders>
-        <AuthenticatedApp />
-      </AuthenticatedAppProviders>
-    ) : (
-      <UnathenticatedApp />
-    )
-    // </React.Suspense>
+  return user ? (
+    <AuthenticatedAppProviders>
+      <AuthenticatedApp />
+    </AuthenticatedAppProviders>
+  ) : (
+    <UnathenticatedApp />
   );
 }
 
---
+++ src/app/authenticated-app/index.js
@@ -0,0 +1,1 @@
+export { AuthenticatedApp } from './authenticated-app';
